426045e9854d2785327d06c198535cf71471be26
Conrad Li <zli3@bowdoin.edu>
2020-04-08 22:56:19 -0400

    fixed nav bar error + button error

--- js/main.js
+++ js/main.js
@@ -1,44 +1,47 @@
+if (document.getElementById("PWMRequestForm")) {
+  // Get the modal
+  var PWMrequestForm = document.getElementById("PWMRequestForm");
 
-// Get the modal
-var PWMrequestForm = document.getElementById("PWMRequestForm");
+  // Get the button that opens the modal
+  var btnMakePWMRequest = document.getElementById("btnMakePWMRequest");
 
-// Get the button that opens the modal
-var btnMakePWMRequest = document.getElementById("btnMakePWMRequest");
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-btnMakePWMRequest.onclick = function() {
-  PWMrequestForm.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  PWMrequestForm.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == this.PWMrequestForm) {
-    this.PWMrequestForm.style.display = "none";
+  // When the user clicks on the button, open the modal
+  btnMakePWMRequest.onclick = function () {
+    PWMrequestForm.style.display = "block";
   }
-}
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function () {
+    PWMrequestForm.style.display = "none";
+  }
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function (event) {
+    if (event.target == this.PWMrequestForm) {
+      this.PWMrequestForm.style.display = "none";
+    }
+  }
+
+};
+
 
 
 $(document).ready(function () {
   $('#nav-placeholder').load("navbar.html", function (response, status) {
-      if (status === 'error') {
-        alert("Failed to load navbar.html");
-      } 
-    });
+    if (status === 'error') {
+      alert("Failed to load navbar.html");
+    }
+  });
 });
 
-    
+
 $(document).ready(function () {
   $('#PWM-request-form').load("requestFormPWM.html", function (response, status) {
-      if (status === 'error') {
-        alert("Failed to load navbar.html");
-      } 
-    });
+    if (status === 'error') {
+      alert("Failed to load requestFormPWM.html");
+    }
+  });
 });
